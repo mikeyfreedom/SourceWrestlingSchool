4900ae219a8db0771bb11f0d65832f4f3a954531
mikeyfreedom <30193897@cityofglacol.ac.uk>
2017-04-27 16:13:29 +0100

    Added a view model to be used for the student profile pages. Will require an updated user model.

---
+++ SourceWrestlingSchool/Models/ProfileViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceWrestlingSchool.Models
+{
+    public class ProfileViewModel
+    {
+        public string ProfileImageFileName { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public DateTime DateJoinedSchool { get; set; }
+        public ClassLevel ClassLevel { get; set; }
+        public string FacebookURL { get; set; }
+        public string TwitterURL { get; set; }
+        public string InstagramURL { get; set; }
+        public string EmailAddress { get; set; }
+        public string BioContent { get; set; }
+        public string YoutubeEmbedLink { get; set; }
+        public string[] SlideshowImageFileNames { get; set; }
+    }
+}
