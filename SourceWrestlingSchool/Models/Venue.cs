2d5e642761073221dd38d07d86f04d47a8413144
mikeyfreedom <30193897@cityofglacol.ac.uk>
2017-05-09 19:20:01 +0100

    Small update to include Venue and subscription agreement page in the branch

---
+++ SourceWrestlingSchool/Models/Venue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceWrestlingSchool.Models
+{
+    public class Venue
+    {
+        [Key]
+        public int VenueID { get; set; }
+        [DisplayName("Venue")]
+        public string VenueName { get; set; }
+        [DisplayName("Location")]
+        public string VenueLocation { get; set; }
+        [DisplayName("Venue Address")]
+        public string VenueAddress { get; set; }
+        [DisplayName("Venue Postcode")]
+        public string VenuePostcode { get; set; }
+        [DisplayName("No of Seats")]
+        public int NoOfSeats { get; set; }
+        public Venue()
+        {
+            Events = new List<LiveEvent>();
+        }
+        public virtual ICollection<LiveEvent> Events { get; set; }
+
+    }
+}
